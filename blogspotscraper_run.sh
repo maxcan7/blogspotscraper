e96471999ad17869a23d92c0e8058e269406cb04
maxcan7 <maxcan7@gmail.com>
2019-06-10 15:55:34 -0400

    fixed url bash variable

--- blogspotscraper_run.sh
+++ blogspotscraper_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-url = ''
+url=''
 ./src/blogspotscraper.py $url
--- src/blogspotscraper.py
+++ src/blogspotscraper.py
@@ -1,12 +1,5 @@
 """
 This script scrapes a Blogspot blog by iterating back in its history.
-
-Usage:
-    1. Set url variable to the Blogspot URL you want as point of departure.
-    2. Press CTRL-C when you want to stop it.
-
-Note: Your IP-number may be temporarily banned from the Blogger service if over-used.
-Use on your own risk.
 """
 
 import requests
